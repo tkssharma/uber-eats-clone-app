d17e12ccd83ee12c064fbd432fab71389d0e16b1
tkssharma <tarun.softengg@gmail.com>
2023-01-29 04:49:10 +0530

    feat(github workflow added): workflows

--- packages/uber-eats/pages/index.tsx
+++ packages/uber-eats/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import React from 'react';
 
 export default function Home() {
   return (
--- packages/uber-eats/pages/signup.tsx
+++ packages/uber-eats/pages/signup.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useForm } from "react-hook-form";
 import { Button } from "../components/common/button";
 import { FormError } from "../components/common/form-error";
