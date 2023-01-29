f1cbc271ba96870003838c731996297254109e19
tkssharma <tarun.softengg@gmail.com>
2023-01-29 10:09:43 +0530

    feat(ci fixes): cI fixes

--- packages/uber-eats/pages/api/hello.ts
+++ packages/uber-eats/pages/api/hello.ts
@@ -1,3 +1,13 @@
-export default (req, res) => {
-  res.status(200).json({ text: "Hello" });
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
 };
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ name: "John Doe" });
+}
